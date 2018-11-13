bf865a60e2bf5f24ae357131b3d1573d3de39647
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-11-14 07:51:35 +0900

    Add project files.

---
+++ SerializeSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializeSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
