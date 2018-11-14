efcae3f0b6f9822a77c54f81371b1032716a5d2b
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-11-14 19:25:39 +0900

    Initial commit

--- SerializeSpike/Program.cs
+++ SerializeSpike/Program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace SerializeSpike
 {
+
+    public class Context
+    {
+        public string ActivityId { get; set; }
+    }
+    [DataContract]
+    public class Message
+    {
+        public Message(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        [DataMember]
+        public int Id { get; private set; }
+        [DataMember]
+        public string Name { get; private set; }
+
+        public int Age { get; set; }
+        [DataMember]
+        public Context Context { get; set; }
+        [DataMember]
+        public IList Books { get; set; }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var message = new Message(1, "ushio");
+            message.Age = 47;
+            message.Context = new Context()
+            {
+                ActivityId = "abc"
+            };
+            message.Books = new List<string>()
+            {
+                "foo", "bar"
+            };
+
+            // No parameter
+            var messageJson = JsonConvert.SerializeObject(message);
+            Console.WriteLine("---- normal");
+            Console.WriteLine(messageJson);
+            var messageJsonWithObjects = JsonConvert.SerializeObject(message, new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            });
+            Console.WriteLine("---- Object");
+            Console.WriteLine(messageJsonWithObjects);
+            var messageJsonWithArrays = JsonConvert.SerializeObject(message, new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Arrays
+            });
+            Console.WriteLine("---- Array");
+            Console.WriteLine(messageJsonWithArrays);
+            var messageJsonWithAll = JsonConvert.SerializeObject(message, new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+            Console.WriteLine("---- All");
+            Console.WriteLine(messageJsonWithAll);
+            var messageJsonWithAuto = JsonConvert.SerializeObject(message, new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            });
+            Console.WriteLine("---- Auto");
+            Console.WriteLine(messageJsonWithAuto);
+            Console.ReadLine();
+
         }
     }
 }
